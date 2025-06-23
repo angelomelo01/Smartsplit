4a9e25f154b559ac5dafe081797e8334b1158b6e
loriziojatwit <lorizioj@wit.edu>
2025-06-23 02:00:27 -0400

    Sign-in/Sign-up Complete

--- mobile/app/(root)/_layout.jsx
+++ mobile/app/(root)/_layout.jsx
@@ -1,12 +1,12 @@
-import { userUser } from "@clerk/clerk-expo";
+import { useUser } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 import { Stack } from "expo-router/stack";
 
 export default function Layout() {
-    const { isSignedIn } = useUser();
+  const { isSignedIn } = useUser();
 
-    if (!isSignedIn) return <Redirect href={"/sign-in"} />;
-    
-    return <Stack screenOptions={{ headerShown: false}} />;
+  if (!isSignedIn) return <Redirect href={"/sign-in"} />;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 
 }
